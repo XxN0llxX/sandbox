c8598aa5dddd054629cf208ec83b6d08b92db130
XxN0llxX <bnoll@sjnma.org>
2017-09-18 13:36:38 -0500

    primitive variable

---
+++ hello.py
@@ -0,0 +1,7 @@
+name=input("please enter your name")
+teacher = "Mr. Gold"
+print("Hello World ")
+print(name)
+
+message = name + "is a great student "+ name.upper() + " tends to agree"
+print(message)
---
+++ math.py
@@ -0,0 +1,20 @@
+x=20+35
+print(x)
+
+y= 3*x + 40
+print (y)
+
+i =50
+print (i)
+i= i + 2
+i=i+10
+print (i)
+radius = 5
+diameter = 2* radius
+circumference=2* 3.145645638476514698743601641*radius
+area=3.145468674598674890567098596720985750989687309867928*radius*radius
+print ("your circumferenceis ")
+print (circumference)
+print ("your area is")
+print (area)
+print ("your radius was "+ str(radius))
